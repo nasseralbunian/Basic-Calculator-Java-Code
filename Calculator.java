408dd495836d57bef709e724b574188a505f21bf
Nasser <n.albunian@gmail.com>
2016-02-02 08:43:04 +0000

    the cacluation code

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String [] args){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter the first number: ");
+		int first = scan.nextInt();
+		
+		System.out.print("Enter the second number: ");
+		int second = scan.nextInt();
+		
+		System.out.println(first + " + " + second + " = " + (first+second));
+		
+		System.out.println(first + " - " + second + " = " + (first-second));
+	}
+}
